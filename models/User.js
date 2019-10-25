15468e5a305845d14b3e37bf7005efc8c4b99b24
YousufQadri <s.yousuf@live.com>
2019-10-26 00:51:15 +0500

    Add: User model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require(mongoose);
+const Schema = mongoose.Schema;
+const validator = require(validator);
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 4,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Error is invalid");
+        }
+      }
+    },
+    contact: {
+      type: Number,
+      required: true,
+      trim: true
+    }
+  },
+  {
+    timestamp: true
+  }
+);
