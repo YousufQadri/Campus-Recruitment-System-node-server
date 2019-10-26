6f4431b35e0520e614164c0df1b73dc5576a374e
YousufQadri <s.yousuf@live.com>
2019-10-27 00:26:06 +0500

    Add: company delete route

--- routes/company.js
+++ routes/company.js
@@ -102,4 +102,39 @@ router.post("/create-company", auth, async (req, res) => {
   }
 });
 
+// @route    DELETE api/v1/company/delete-company
+// @desc     Delete company
+// @access   Private
+router.delete("/delete-company/:id", auth, async (req, res) => {
+  // validate object ID
+  const isValidObjectId = mongoose.Types.ObjectId.isValid(req.params.id);
+  if (!isValidObjectId) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid Object ID" });
+  }
+
+  try {
+    // search company in database
+    const companyToDelete = await Company.findByIdAndDelete(req.params.id);
+    if (!companyToDelete) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Company not found" });
+    }
+
+    return res.json({
+      success: true,
+      message: "Company deleted successfully!"
+    });
+  } catch (error) {
+    console.log("Error:", error.message);
+    res.status(500).json({
+      message: "Internal server error",
+      success: false,
+      error: error.message
+    });
+  }
+});
+
 module.exports = router;
