b0553fac8eb79ac29d792f7a39fb37fafe1b032b
YousufQadri <s.yousuf@live.com>
2019-10-26 17:57:51 +0500

    Add: middleware and use route

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 
 const connectDB = require("./config/db");
+const user = require("./routes/user");
 
+// Connect with DB
 connectDB();
+
+// Middleware
+app.use(express.json({ extender: false }));
+
+// Routes
+app.use("/api/v1/user", user);
 
 // Assign PORT
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log("Server running at port " + PORT));
