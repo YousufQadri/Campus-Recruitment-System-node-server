33ca0e610c137adc6d8fcc498853564e92893cb1
YousufQadri <s.yousuf@live.com>
2019-10-29 11:06:04 +0500

    Refactor: Auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,15 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-const User = require("../models/User");
+const Company = require("../models/Company");
+const Student = require("../models/Student");
+const Admin = require("../models/Admin");
 
 // Environment variable setup
 const JWT_SECRET = process.env.JWT_SECRET || config.get("JWT_SECRET");
 
-module.exports = async (req, res, next) => {
+// Student Authentication Middleware
+const studentAuth = async (req, res, next) => {
   // Grab token from header
   const token = req.header("x-auth-token");
 
@@ -17,21 +20,22 @@ module.exports = async (req, res, next) => {
       message: "No token, authorization denied"
     });
   }
+
   try {
     // Verify token
     const decoded = await jwt.verify(token, JWT_SECRET);
 
-    // Check user in DB
-    let existingUser = await User.findById(decoded.user.id);
-    if (!existingUser) {
+    // Check student in DB
+    let studentExist = await Student.findOne({ _id: decoded.student._id });
+    if (!studentExist) {
       res.status(401).json({
         success: false,
-        message: "Token is not valid"
+        message: "Invalid token!"
       });
     }
 
     // Set use object in req body
-    req.user = decoded.user;
+    req.student = decoded.student;
     next();
   } catch (error) {
     console.log("Error: ", error.message);
@@ -40,4 +44,86 @@ module.exports = async (req, res, next) => {
       message: "Token is not valid"
     });
   }
+};
+
+// Company Authentication Middleware
+const companyAuth = async (req, res, next) => {
+  // Grab token from header
+  const token = req.header("x-auth-token");
+
+  // Check token existense
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "No token, authorization denied"
+    });
+  }
+
+  try {
+    // Verify token
+    const decoded = await jwt.verify(token, JWT_SECRET);
+
+    // Check company in DB
+    let companyExist = await Company.findOne({ _id: decoded.company._id });
+    if (!companyExist) {
+      res.status(401).json({
+        success: false,
+        message: "Invalid token!"
+      });
+    }
+
+    // Set use object in req body
+    req.company = decoded.company;
+    next();
+  } catch (error) {
+    console.log("Error: ", error.message);
+    res.status(401).json({
+      success: false,
+      message: "Token is not valid"
+    });
+  }
+};
+
+// Admin Authentication Middleware
+const adminAuth = async (req, res, next) => {
+  // Grab token from header
+  const token = req.header("x-auth-token");
+
+  // Check token existense
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "No token, authorization denied"
+    });
+  }
+
+  try {
+    // Verify token
+    const decoded = await jwt.verify(token, JWT_SECRET);
+
+    // Check admin in DB
+    let adminExist = await Company.findOne({ _id: decoded.admin._id });
+    if (!adminExist) {
+      res.status(401).json({
+        success: false,
+        message: "Invalid token!"
+      });
+    }
+
+    // Set use object in req body
+    req.admin = decoded.admin;
+    next();
+  } catch (error) {
+    console.log("Error: ", error.message);
+    res.status(401).json({
+      success: false,
+      message: "Token is not valid"
+    });
+  }
+};
+
+module.exports = {
+  studentAuth,
+  companyAuth,
+  adminAuth
 };
