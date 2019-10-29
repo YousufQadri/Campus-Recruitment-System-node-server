b00d53282ea4d9eed939d3d63c056a7bf16d2d23
YousufQadri <s.yousuf@live.com>
2019-10-29 10:06:23 +0500

    Add: Admin model

---
+++ models/Admin.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const validator = require("validator");
+
+const AdminSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 4,
+      trim: true
+    }
+  },
+  {
+    timestamp: true
+  }
+);
+module.exports = mongoose.model("Admin", AdminSchema);
