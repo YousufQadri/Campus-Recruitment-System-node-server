07477299906a4db9909cf0e89d25e4c94c228cb2
YousufQadri <s.yousuf@live.com>
2019-11-08 21:21:45 +0500

    Fix: Delete company route functionality

--- routes/admin.js
+++ routes/admin.js
@@ -108,16 +108,16 @@ router.post("/login", async (req, res) => {
 // @access   Private
 router.get("/get-data", auth.adminAuth, async (req, res) => {
   try {
-    const allStudents = await Student.find({}, { password: 0 });
+    const students = await Student.find({}, { password: 0 });
     const companies = await Company.find({}, { password: 0 });
-    const companyJobs = await Jobs.find({}).populate("companyId");
+    const jobs = await Jobs.find({}).populate("companyId", { password: 0 });
 
     return res.status(200).send({
       success: true,
       message: "Students, jobs and companies record",
-      allStudents,
+      students,
       companies,
-      companyJobs
+      jobs
     });
   } catch (error) {
     return res.status(500).send({
@@ -142,19 +142,24 @@ router.delete("/delete-company/:id", auth.adminAuth, async (req, res) => {
 
   try {
     // search company in database
-    const companyToDelete = await Company.find({ _id: req.params.id });
-    console.log(req.params.id);
+    const companyToDelete = await Company.findByIdAndDelete(req.params.id);
     console.log(companyToDelete);
 
-    if (!companyToDelete) {
+    if (companyToDelete === null) {
       return res
         .status(400)
         .json({ success: false, message: "Company not found" });
     }
+    const companyJobs = await Jobs.deleteMany({
+      companyId: req.params.id
+    });
+    console.log(companyJobs);
+
+    console.log(companyJobs);
 
     return res.json({
       success: true,
-      message: "Company deleted successfully!"
+      message: "Company and jobs deleted successfully!"
     });
   } catch (error) {
     console.log("Error:", error.message);
@@ -164,6 +169,31 @@ router.delete("/delete-company/:id", auth.adminAuth, async (req, res) => {
       error: error.message
     });
   }
+  // try {
+  //   // search company in database
+  //   const companyToDelete = await Company.find({ _id: req.params.id });
+  //   const companyJobs
+  //   console.log(req.params.id);
+  //   console.log(companyToDelete);
+
+  //   if (!companyToDelete) {
+  //     return res
+  //       .status(400)
+  //       .json({ success: false, message: "Company not found" });
+  //   }
+
+  //   return res.json({
+  //     success: true,
+  //     message: "Company deleted successfully!"
+  //   });
+  // } catch (error) {
+  //   console.log("Error:", error.message);
+  //   res.status(500).json({
+  //     message: "Internal server error",
+  //     success: false,
+  //     error: error.message
+  //   });
+  // }
 });
 
 module.exports = router;
